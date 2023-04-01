63fc978ced8396e21349030c6891e9a6361e6fbc
Gijs Blanken <27724177+gijs-blanken@users.noreply.github.com>
2023-04-01 17:53:28 +0200

    Actually used the migrate func (#12)

--- submit-hs/app/Database/Db.hs
+++ submit-hs/app/Database/Db.hs
@@ -7,6 +7,9 @@ import Database.Beam
 import Database.Model
 import Database.Beam.Migrate
 import Database.Beam.Sqlite
+import Database.SQLite.Simple
+import Database.Beam.Migrate.Simple
+import Database.Beam.Sqlite.Migrate
 
 data SubmitDb f = SubmitDb
   { _users :: f (TableEntity UserT),
@@ -18,3 +21,20 @@ data SubmitDb f = SubmitDb
     _gradings :: f (TableEntity GradingT)
   }
   deriving (Generic, Database be)
+
+submitDb :: DatabaseSettings Sqlite SubmitDb
+submitDb = unCheckDatabase checkedSubmitDb
+
+getSubmit :: Connection -> IO [User]
+getSubmit conn = do
+  runBeamSqlite conn $ do
+    runSelectReturningList $ select (all_ (_users submitDb))
+
+checkedSubmitDb :: CheckedDatabaseSettings Sqlite SubmitDb
+checkedSubmitDb = defaultMigratableDbSettings
+
+migrateDb :: IO ()
+migrateDb = do
+  conn <- liftIO $ open "database.db"
+  liftIO $ runBeamSqliteDebug putStrLn conn migrate
+  where migrate = autoMigrate migrationBackend checkedSubmitDb
