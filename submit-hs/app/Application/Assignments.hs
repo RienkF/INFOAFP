d697b250f2a9af7e29a9ee8f3052d1c038312894
Rienk <rienkfidder@gmail.com>
2023-04-17 16:13:18 +0200

    Removed trace

--- submit-hs/app/Application/Assignments.hs
+++ submit-hs/app/Application/Assignments.hs
@@ -1,16 +1,15 @@
 module Application.Assignments where
 
+import Api.Types.AssignmentTypes
+import Api.Types.ClassroomTypes
+import Data.Maybe
+import qualified Database.Assignments
 import Database.Beam
 import qualified Database.Classrooms
 import Database.Model
 import qualified Database.Model as Database
 import qualified Database.Users
 import Servant
-import Api.Types.ClassroomTypes
-import Api.Types.AssignmentTypes
-import qualified Database.Assignments
-import Debug.Trace
-import Data.Maybe
 
 getAssignments :: IO [Assignment]
 getAssignments = Database.Assignments.getAssignments Nothing
@@ -19,13 +18,13 @@ addAssignment :: AddAssignmentBody -> Handler (Maybe Assignment)
 addAssignment body = do
   classrooms <- liftIO $ Database.Classrooms.getClassrooms $ Just [assignmentClassroom body]
 
-  t <- case classrooms of
-    [classroom] -> liftIO $ Database.Assignments.addAssignment
-        (assignmentStartDate body)
-        (assignmentDeadline body)
-        (assignmentDescription body)
-        (assignmentWeight body)
-        classroom
+  case classrooms of
+    [classroom] ->
+      liftIO $
+        Database.Assignments.addAssignment
+          (assignmentStartDate body)
+          (assignmentDeadline body)
+          (assignmentDescription body)
+          (assignmentWeight body)
+          classroom
     _ -> return Nothing
-
-  trace ("id: " ++ show (_assignmentId $ fromJust t)) $ return t
--- submit-hs/app/Database/Assignments.hs
+++ submit-hs/app/Database/Assignments.hs
@@ -1,25 +1,25 @@
 module Database.Assignments where
 
+import Data.Text
+import Data.Time (LocalTime)
 import Database.Beam
 import Database.Beam.Sqlite
-import Database.Db (SubmitDb (assignments), databaseConnection, submitDb, classrooms)
+import Database.Db (SubmitDb (assignments), classrooms, databaseConnection, submitDb)
 import Database.Model
-import Data.Time (LocalTime)
-import Data.Text
-import Debug.Trace
 
 getAssignments :: Maybe [Int] -> IO [Assignment]
 getAssignments assignmentFilter = do
   conn <- databaseConnection
   runBeamSqlite conn $ do
-    runSelectReturningList $ select 
-      ( filter_
-          ( \assignment -> case assignmentFilter of
-              Just filter -> _assignmentId assignment `in_` Prelude.map fromIntegral filter
-              Nothing -> val_ True
-          )
-          $ all_ (assignments submitDb)
-      )
+    runSelectReturningList $
+      select
+        ( filter_
+            ( \assignment -> case assignmentFilter of
+                Just filter -> _assignmentId assignment `in_` Prelude.map fromIntegral filter
+                Nothing -> val_ True
+            )
+            $ all_ (assignments submitDb)
+        )
 
 addAssignment :: LocalTime -> LocalTime -> Text -> Double -> Classroom -> IO (Maybe Assignment)
 addAssignment startDate deadline description weight classroom = do
@@ -28,14 +28,15 @@ addAssignment startDate deadline description weight classroom = do
     result <-
       runInsertReturningList $
         insert (assignments submitDb) $
-          insertExpressions [Assignment 
-            default_  
-            (val_ startDate) 
-            (val_ deadline) 
-            (val_ description) 
-            (val_ weight) 
-            (val_ $ pk classroom)
-          ]
+          insertExpressions
+            [ Assignment
+                default_
+                (val_ startDate)
+                (val_ deadline)
+                (val_ description)
+                (val_ weight)
+                (val_ $ pk classroom)
+            ]
     -- This should be only 1
     return
       ( case result of
